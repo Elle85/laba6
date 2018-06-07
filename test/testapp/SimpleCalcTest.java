0f9f31dc5adec1faaa81b7c5bec3a84eda2c45cb
XACAH N3 XACAB|-0PTA <dirtforever1@gmail.com>
2018-06-07 20:39:10 +0300

    test

--- test/testapp/SimpleCalcTest.java
+++ test/testapp/SimpleCalcTest.java
@@ -1,86 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package testapp;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-
-/**
- *
- * @author Dima
- */
 public class SimpleCalcTest {
-    
-    public SimpleCalcTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
 
-    /**
-     * Test of getSum method, of class SimpleCalc.
-     */
-    @Test
-    public void testGetSum() {
-        System.out.println("getSum");
-        SimpleCalc instance = null;
-        double expResult = 0.0;
-        double result = instance.getSum();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+int p1=4;
+int p2=0;
+SimpleCalc sc=new SimpleCalc(p1,p2);;
 
-    /**
-     * Test of getDiff method, of class SimpleCalc.
-     */
-    @Test
-    public void testGetDiff() {
-        System.out.println("getDiff");
-        SimpleCalc instance = null;
-        double expResult = 0.0;
-        double result = instance.getDiff();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+public SimpleCalcTest() {
+}
 
-    /**
-     * Test of getMult method, of class SimpleCalc.
-     */
-    @Test
-    public void testGetMult() {
-        System.out.println("getMult");
-        SimpleCalc instance = null;
-        double expResult = 0.0;
-        double result = instance.getMult();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+@BeforeClass
+public static void setUpClass() {
+System.out.println("BeforeClass");
+}
 
-    /**
-     * Test of getDiv method, of class SimpleCalc.
-     */
-    @Test
-    public void testGetDiv() {
-        System.out.println("getDiv");
-        SimpleCalc instance = null;
-        double expResult = 0.0;
-        double result = instance.getDiv();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
+@AfterClass
+public static void tearDownClass() {
+System.out.println("AfterClass");
+}
+/**
+* Test of getSum method, of class SimpleCalc.
+
+Лабораторна робота No5. Технології програмування
+*/
+@Test
+public void testGetSum() {
+System.out.println("getSum tested");
+assertEquals(5, sc.getSum(),0.0);
+}
+/**
+* Test of getDiff method, of class SimpleCalc.
+*/
+@Test
+public void testGetDiff() {
+System.out.println("getDiff tested");
+assertEquals(5,sc.getDiff(),0.0);
+}
+/**
+* Test of getMult method, of class SimpleCalc.
+*/
+@Test
+public void testGetMult() {
+System.out.println("getMult tested");
+assertEquals(0,sc.getMult(),0.0);
+}
+/**
+* Test of getDiv method, of class SimpleCalc.
+*/
+@Test(expected = ArithmeticException.class)
+public void testGetDiv(){
+System.out.println("getDiv tested");
+sc.getDiv();
+}
 }
